e287486d9fb34200b561bea205fa8897869a6645
shivaprasad123k <92025182+shivaprasad123k@users.noreply.github.com>
2021-10-30 20:32:22 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -6,7 +6,7 @@ I only work on <a href='https://t.me/moviehubgroupp'>MOVIE HUB GROUP 😌</a>
 
 ©️ MᴀɪɴᴛᴀɪɴᴇD Bʏ  <a href='https://t.me/OGGY123kph'>𝑺ʜɪᴠᴀ 🕵️‍♂️</a></b>"""
     HELP_TXT = """𝙷𝙴𝚈 {}
-HEY {} DUDE IAM WORK ONLY FOR MY BOSS😎."""
+𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """➪ 𝑴𝒚 𝒏𝒂𝒎𝒆: 𝑹ᴇxɪᴇ
 ➪ 𝑪𝒓𝒆𝒂𝒕𝒐𝒓 : [𝑺𝒉𝒊𝒗𝒂](https://t.me/OGGY123kph)
 ➪ 𝑳𝒊𝒃𝒓𝒂𝒓𝒚 : 𝑷ʏʀᴏɢʀᴀᴍ
