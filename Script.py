52ec9a7ef9fb2d115b7663029d1b161c706ddf62
shivaprasad123k <92025182+shivaprasad123k@users.noreply.github.com>
2021-10-30 22:12:55 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -7,7 +7,7 @@ I only work on <a href='https://t.me/moviehubgroupp'>MOVIE HUB GROUP 😌</a>
 ©️ MᴀɪɴᴛᴀɪɴᴇD Bʏ  <a href='https://t.me/OGGY123kph'>𝑺ʜɪᴠᴀ 🕵️‍♂️</a></b>"""
     HELP_TXT = """HEY {}
 YOU CAN'T USE ME BECAUSE IAM A PRIVET CAT."""
-    ABOUT_TXT = """😒 HEY (message.from.user.mension) DUDE 🚶‍♂️⚠️
+    ABOUT_TXT = """😒 HEY {message.from.user.mension} DUDE 🚶‍♂️⚠️
 
 ➪ 𝑴𝒚 𝒏𝒂𝒎𝒆 : 𝑹ᴇxɪᴇ
 ➪ <b>𝑪𝒓𝒆𝒂𝒕𝒐𝒓 : <a href='https://t.me/OGGY123kph'>𝑺ʜɪᴠᴀ 🕵️‍♂️</a>
