f84b3437682b9e0b8cdf4b08c8abaf9f3c38a27a
shivaprasad123k <92025182+shivaprasad123k@users.noreply.github.com>
2021-11-01 17:40:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -35,7 +35,9 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('⚡ WORKING GROUP ⚡', url=f'https://t.me/moviehubgroupp')
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            ],[
+            InlineKeyboardButton('GROUP 📂', url=f'https://t.me/moviehubgroupp')
             ],[
             InlineKeyboardButton('Help ⚙️', callback_data='help'),
             InlineKeyboardButton('About 😎', callback_data='about')
