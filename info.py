a327b98abfbf3fdfef03a909afd664d741e0571e
xxxᴛᴇɴᴛᴀᴄɪᴏɴ <81632023+xxxtentacion-TG@users.noreply.github.com>
2021-11-08 21:59:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,6 +30,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
+MAX_LIST_ELM = None
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TeamEvamaria')
 P_TTTI_SHOW_OFF = eval((environ.get('P_TTTI_SHOW_OFF', "False")))
