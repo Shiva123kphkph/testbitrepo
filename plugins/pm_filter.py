cc6afb4fafadc37d533d436e62acfdbcaed50755
Shiva <90966061+Shiva123kphkph@users.noreply.github.com>
2021-11-10 16:45:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1,13 +1,20 @@
 #Kanged From @TroJanZheX
-import asyncio
+# Made with python3
+# (C) @FayasNoushad
+# Copyright permission under MIT License
+# All rights reserved by FayasNoushad
+# License -> https://github.com/FayasNoushad/Calculator-Bot/blob/main/LICENSEimport asyncio
 import re
 import ast
+import os
 from Script import script
 import pyrogram
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, make_inactive
 from info import ADMINS, AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, AUTH_GROUPS, P_TTTI_SHOW_OFF, IMDB, SINGLE_BUTTON, LONG_IMDB_DESCRIPTION
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, filters
+from pyrogram import Client, filters
+from pyrogram.types import *
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from utils import get_size, is_subscribed, get_poster, temp
 from database.users_chats_db import db
@@ -18,7 +25,119 @@ from database.filters_mdb import(
    get_filters,
 )
 
+Bot = Client(
+    "Calculator Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
 BUTTONS = {}
+
+CALCULATE_TEXT = "Made by @NAZRIYAOFFTOPIC"
+CALCULATE_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton("DEL", callback_data="DEL"),
+        InlineKeyboardButton("AC", callback_data="AC"),
+        InlineKeyboardButton("(", callback_data="("),
+        InlineKeyboardButton(")", callback_data=")")
+        ],[
+        InlineKeyboardButton("7", callback_data="7"),
+        InlineKeyboardButton("8", callback_data="8"),
+        InlineKeyboardButton("9", callback_data="9"),
+        InlineKeyboardButton("÷", callback_data="/")
+        ],[
+        InlineKeyboardButton("4", callback_data="4"),
+        InlineKeyboardButton("5", callback_data="5"),
+        InlineKeyboardButton("6", callback_data="6"),
+        InlineKeyboardButton("×", callback_data="*")
+        ],[
+        InlineKeyboardButton("1", callback_data="1"),
+        InlineKeyboardButton("2", callback_data="2"),
+        InlineKeyboardButton("3", callback_data="3"),
+        InlineKeyboardButton("-", callback_data="-"),
+        ],[
+        InlineKeyboardButton(".", callback_data="."),
+        InlineKeyboardButton("0", callback_data="0"),
+        InlineKeyboardButton("=", callback_data="="),
+        InlineKeyboardButton("+", callback_data="+"),
+        ]]
+    )
+
+
+
+@Bot.on_message(filters.private & filters.command(["calc", "calculate", "calculator"]))
+async def calculate(bot, update):
+    await update.reply_text(
+        text=CALCULATE_TEXT,
+        reply_markup=CALCULATE_BUTTONS,
+        disable_web_page_preview=True,
+        quote=True
+    )
+
+
+@Bot.on_callback_query()
+async def cb_data(bot, update):
+        data = update.data
+        try:
+            message_text = update.message.text.split("\n")[0].strip().split("=")[0].strip()
+            message_text = '' if CALCULATE_TEXT in message_text else message_text
+            if data == "=":
+                text = float(eval(message_text))
+            elif data == "DEL":
+                text = message_text[:-1]
+            elif data == "AC":
+                text = ""
+            else:
+                text = message_text + data
+            await update.message.edit_text(
+                text=f"{text}\n\n{CALCULATE_TEXT}",
+                disable_web_page_preview=True,
+                reply_markup=CALCULATE_BUTTONS
+            )
+        except Exception as error:
+            print(error)
+
+
+@Bot.on_inline_query()
+async def inline(bot, update):
+    if len(update.data) == 0:
+        try:
+            answers = [
+                InlineQueryResultArticle(
+                    title="Calculator",
+                    description=f"New calculator",
+                    input_message_content=InputTextMessageContent(
+                        text=CALCULATE_TEXT,
+                        disable_web_page_preview=True
+                    ),
+                    reply_markup=CALCULATE_BUTTONS
+                )
+            ]
+        except Exception as error:
+            print(error)
+    else:
+        try:
+            message_text = update.message.text.split("\n")[0].strip().split("=")[0].strip()
+            data = message_text.replace("×", "*").replace("÷", "/")
+            text = float(eval(data))
+            answers = [
+                InlineQueryResultArticle(
+                    title="Answer",
+                    description=f"Results of your input",
+                    input_message_content=InputTextMessageContent(
+                        text=f"{data} = {text}",
+                        disable_web_page_preview=True
+                    )
+                )
+            ]
+        except:
+            pass
+    await update.answer(answers)
+
+
+Bot.run()
+
 
 PIN_TEXT = """<b>𝖯𝗂𝗇:  
 𝖠𝗅𝗅 𝗍𝗁𝖾 𝗉𝗂𝗇 𝗋𝖾𝗅𝖺𝗍𝖾𝖽 𝖼𝗈𝗆𝗆𝖺𝗇𝖽𝗌 𝖼𝖺𝗇 𝖻𝖾 𝖿𝗈𝗎𝗇𝖽 𝗁𝖾𝗋𝖾; 𝗄𝖾𝖾𝗉 𝗒𝗈𝗎𝗋 𝖼𝗁𝖺𝗍 𝗎𝗉 𝗍𝗈 𝖽𝖺𝗍𝖾 𝗈𝗇 𝗍𝗁𝖾 𝗅𝖺𝗍𝖾𝗌𝗍 𝗇𝖾𝗐𝗌 𝗐𝗂𝗍𝗁 𝖺 𝗌𝗂𝗆𝗉𝗅𝖾 𝗉𝗂𝗇𝗇𝖾𝖽 𝗆𝖾𝗌𝗌𝖺𝗀𝖾!  
